a1eeb02c5343f03ae1ed294ab1e3aa9c8c1048da
Alla <molochko.1979@mail.ru>
2023-03-22 21:40:56 +0300

    add Example + 003 деление

--- Exemple_003_Sum/Program.cs
+++ Exemple_003_Sum/Program.cs
@@ -1,3 +1,4 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10); //1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(result);
